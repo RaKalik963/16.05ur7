989d12ce1901100171b0e5daac3545f0f8d3ee3e
RaKalik963 <andrejmahot646@gmail.com>
2024-05-16 20:21:16 +0300

    dz-ex1

--- main.cpp
+++ main.cpp
@@ -4,18 +4,27 @@ using namespace std;
 
 int main()
 {
-	for (int i = 0; i < 5; i++)
+	cout << "enter column";
+	int col;
+	cin >> col;
+	cout << "enter row";
+	int row;
+	cin >> row;
+	for (size_t i = 0; i < col; i++)
 	{
-		for (int j = 0; j < 5; j++)
+		for (size_t j = 0; j < row; j++)
 		{
-			if (i - j <= 4 && i <= j)
+			if (i <= j)
+			{
 				cout << "*";
+			}
 			else
+			{
 				cout << " ";
-		}
-		cout << endl;
+			}
+			
+		}cout << endl;
 	}
-
 	system("pause");
 	return 0;
 }
