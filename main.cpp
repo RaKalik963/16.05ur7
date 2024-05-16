aa165501c08469a05751968d7500e59c10a1500b
RaKalik963 <andrejmahot646@gmail.com>
2024-05-16 20:03:16 +0300

    dz-ex4

--- main.cpp
+++ main.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main()
 {
-
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (i + j >= 4 && i >= j)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
 	system("pause");
 	return 0;
 }
