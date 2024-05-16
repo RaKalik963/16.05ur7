9150f3022cbba957e7c9c36245c7808aab5cc2e7
RaKalik963 <andrejmahot646@gmail.com>
2024-05-16 20:18:26 +0300

    dz-ex222222222

--- main.cpp
+++ main.cpp
@@ -4,7 +4,27 @@ using namespace std;
 
 int main()
 {
-
+	cout << "enter column";
+	int col;
+	cin >> col;
+	cout << "enter row";
+	int row;
+	cin >> row;
+	for (size_t i = 0; i < col; i++)
+	{
+		for (size_t j = 0; j < row; j++)
+		{
+			if (i <= j)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+			cout << endl;
+		}
+	}
 	system("pause");
 	return 0;
 }
