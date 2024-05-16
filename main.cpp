d992d41473c60ed7fa66c68d4a9d1f25ac733711
RaKalik963 <andrejmahot646@gmail.com>
2024-05-16 19:43:01 +0300

    21331231231231232131212312312

--- main.cpp
+++ main.cpp
@@ -4,7 +4,30 @@ using namespace std;
 
 int main()
 {
+	cout << "enter start of diapasone";
+	int d1;
+	cin >> d1;
+	cout << "enter end of diapasone";
+	int d2;
+	cin >> d2;
+	cout << "enter num";
+	int numb;
+	cin >> numb;
 
-	system("pause");
+	for (;numb <= d2 && numb >= d1;)
+	{
+		if (numb > d2 || numb < d1)
+		{
+			cout << "no" << endl;
+			cout << "enter num";
+			cin >> numb;
+		}
+		if (numb <= d2 && numb >= d1)
+		{
+			cout << "yes" << endl;
+			return 0;
+		}
+	}
 	return 0;
+	//system("pause");
 }
